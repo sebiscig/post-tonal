578b852cd995cb26592d35e0159d8c8310f9fdd8
sebiscig <sebastian.bisciglia@gmail.com>
2019-03-08 17:20:24 -0500

    Updates
    1. Updating some packages; 2. shutting down two rows

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
-    blacklistedRows : [],
+    blacklistedRows : ['7436152et098','8975e423t106'],
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
